de2950f8232081c47240d745ca996cff6985b319
MonStar1 <mazaxict1@gmail.com>
2013-09-25 21:58:56 +0300

    Added models: computer, processor, motherboard, psu, cpu. Create you models of the example

---
+++ dot_net_c_sharp_project/ComputerModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_net_c_sharp_project
+{
+    class ComputerModel
+    {
+        private long id;
+
+        private String name;
+
+        private double price;
+
+        private MotherboardModel motherboard;
+
+        private ProcessorModel processor;
+
+        private PSUModel psu;
+
+        //-----TODO: add new model of example
+
+        //private VideocardModel video;
+        //private KeyboardModel keyboard;
+        //private MouseModel mouse;
+        //pribate HDDModel hdd;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public MotherboardModel Motherboard
+        {
+            get { return motherboard; }
+            set { motherboard = value; }
+        }
+
+        public ProcessorModel Processor
+        {
+            get { return processor; }
+            set { processor = value; }
+        }
+
+        public PSUModel Psu
+        {
+            get { return psu; }
+            set { psu = value; }
+        }
+    }
+}
---
+++ dot_net_c_sharp_project/MotherboardModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_net_c_sharp_project
+{
+    class MotherboardModel
+    {
+        private long id;
+
+        private String name;
+
+        private double price;
+
+        private String manufacturer;
+
+        private String model;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public String Manufacturer
+        {
+            get { return manufacturer; }
+            set { manufacturer = value; }
+        }
+
+        public String Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+    }
+}
