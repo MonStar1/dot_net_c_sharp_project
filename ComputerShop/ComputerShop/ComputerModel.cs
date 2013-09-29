60fbca8b8200ccdef9a9e6cbadca088d55add93c
HeyDafna <kozich-nastia@mail.ru>
2013-09-30 01:50:59 +0300

    Add new classes

---
+++ ComputerShop/ComputerShop/ComputerModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop
+{
+    class ComputerModel
+    {
+        private long id;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private List<long> id_products = new List<long>();
+        public List<long> Id_products
+        {
+            get { return id_products; }
+            set { id_products = value; }
+        }
+
+        public void addId_products(long id)
+        {
+            id_products.Add(id);
+        }
+
+        public bool removeId_products(long id)
+        {
+            return id_products.Remove(id);
+        }
+        private String description;
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+}
---
+++ ComputerShop/ComputerShop/UserModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop
+{
+    class UserModel
+    {
+        private long id;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private String firstName;
+
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        private String lastName;
+
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        private String login;
+        public String Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        private String password;
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        private long id_role;
+        public long Id_role
+        {
+            get { return id_role; }
+            set { id_role = value; }
+        }
+        }
+}
